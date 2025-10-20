263ccee26deb8fe58d2b08d7aba0c1bd436291df
lusacramento <lucianossacramento@yahoo.com.br>
2025-10-20 15:59:54 -0300

    configure host and ports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/test-utils'
-  ]
-})
+  runtimeConfig: {
+    public: {
+      devPort: process.env.DEV_PORT,
+      host: process.env.HOST,
+    },
+  },
+
+  devServer: {
+    port: process.env.DEV_PORT ? parseInt(process.env.DEV_PORT) : 8081,
+  },
+
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/test-utils"],
+});
