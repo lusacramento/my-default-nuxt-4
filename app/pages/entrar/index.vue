129a76663b2881e059144a5735b5d3e2fc5124f2
lusacramento <lucianossacramento@yahoo.com.br>
2025-11-14 13:06:09 -0300

    feat implents register confirm

---
+++ app/pages/entrar/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Container>
+    <h1>Entrar</h1>
+  </Container>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ server/api/auth/signup/index.get.ts
@@ -0,0 +1,52 @@
+import { useSecurity } from "~/composables/domain/security";
+import { UserSchema } from "~~/server/models/user.schema";
+import { Messages } from "~~/types/enums/Messages";
+
+export default defineEventHandler(async (event) => {
+  const { token } = await getQuery(event);
+
+  try {
+    if (!token)
+      throw createError({
+        statusCode: 400,
+        statusMessage: "Bad Request",
+        message: Messages.TOKEN_NOT_PROVIDER,
+      });
+
+    // Token verification
+    const { error, decoded } = useSecurity().decodeToken(
+      token as string,
+      useRuntimeConfig().secret
+    );
+
+    if (error && error?.name !== "TokenExpiredError")
+      throw createError({
+        statusCode: 400,
+        statusMessage: "Bad credentials",
+        message: Messages.INVALID_CREDENTIALS,
+      });
+
+    const payload = decoded as { id: string; email: string; password: string };
+
+    const user = await UserSchema.findById(payload.id);
+
+    if(!user)
+      throw createError({
+        statusCode: 400,
+        statusMessage: "Bad Request",
+        message: Messages.INVALID_CREDENTIALS,
+      });
+
+    if (user && !user.isVerified) {
+      await UserSchema.findByIdAndUpdate(user._id, {
+        isVerified: true,
+      });
+
+    }
+      sendRedirect(event, `/entrar?email=${user.email}&eVerificado=true`);
+      return;
+      
+  } catch (error) {
+    return error;
+  }
+});
