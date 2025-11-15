9b0fcd4b79474ce7a091a6423ce441ec798b4fc4
lusacramento <lucianossacramento@yahoo.com.br>
2025-11-15 15:04:13 -0300

    feat implements header

---
+++ app/components/layouts/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <Container class="mt-5 mb-3 text-center">
+    <h1>{{ props.pageName }}</h1>
+  </Container>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps<{ pageName: string }>()
+</script>
+
+<style>
+
+</style>
