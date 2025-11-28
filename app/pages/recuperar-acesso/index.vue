5ca00f823523b99bfd0a284788f79e6671ff0077
lusacramento <lucianossacramento@yahoo.com.br>
2025-11-28 13:43:52 -0300

    feat(recuperar-acesso): integrate toast notifications for error and success messages during access recovery

--- app/pages/recuperar-acesso/index.vue
+++ app/pages/recuperar-acesso/index.vue
@@ -4,12 +4,12 @@
     <AuthRescueAccessForm @rescue="rescueAccess" />
     <Row class="text-center mb-1 mt-3">
       <Col>
-        <NuxtLink to="/registrar">Não tem conta?</NuxtLink>
+      <NuxtLink to="/registrar">Não tem conta?</NuxtLink>
       </Col>
     </Row>
     <Row class="text-center">
       <Col>
-        <NuxtLink to="/entrar">Já tem conta?</NuxtLink>
+      <NuxtLink to="/entrar">Já tem conta?</NuxtLink>
       </Col>
     </Row>
   </Container>
@@ -17,19 +17,28 @@
 
 <script lang="ts" setup>
 import { useIAuth } from '~/composables/interfaces/iAuth';
+import { useMyToastStore } from '~/stores/toast';
 import type { AuthResponse } from '~~/types/AuthResponse';
 
 const pageName = "Recuperar Acesso"
+
+const toast = useMyToastStore()
+
+onMounted(() => {
+  if (toast.isLoaded) toast.show();
+});
 
 async function rescueAccess(email: string) {
   const { error, message } = await useIAuth().rescueAccess(email) as AuthResponse;
 
   if (error) {
-    console.error(error.message)
+    await toast.load(error.message, 'erro')
+    toast.show()
   }
 
- if(message){
-  console.info(message)
- }
+  if (message) {
+    await toast.load(message, 'sucesso')
+    useRouter().push('/')
+  }
 }
 </script>
