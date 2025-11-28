f24c127a3a9bf83f6fdcbc8458249f44b0aeb004
lusacramento <lucianossacramento@yahoo.com.br>
2025-11-28 13:43:37 -0300

    feat(projetos): add projetos page with toast notification integration

---
+++ app/pages/projetos/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Container>
+    <LayoutsHeader :page-name="pageName" />
+    <TemplatesToast id="projetos-toast" />
+  </Container>
+</template>
+
+<script lang="ts" setup>
+import { useMyToastStore } from '~/stores/toast';
+
+const pageName = 'Projetos'
+
+const toast = useMyToastStore();
+
+onMounted(() => {
+  if (toast.isLoaded) toast.show();
+}); 
+</script>
