66ad101c27bbfdec631edb0d3795a97555236f9f
lusacramento <lucianossacramento@yahoo.com.br>
2025-11-28 13:40:27 -0300

    feat(toast): enhance index page to display toast notifications on load

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,17 @@
 <template>
     <Container>
         <LayoutsHeader :page-name="pageName" />
+        <TemplatesToast id="index-toast" />
     </Container>
 </template>
 
 <script lang="ts" setup>
-    const pageName = useRuntimeConfig().public.appName
+import { useMyToastStore } from '~/stores/toast';
+
+const pageName = useRuntimeConfig().public.appName
+const toast = useMyToastStore();
+
+onMounted(() => {
+  if (toast.isLoaded) toast.show();
+});
 </script>
