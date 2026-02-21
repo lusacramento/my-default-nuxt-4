88c6ae5072324caf84c6d1ff129b9973484c8855
lusacramento <lucianossacramento@yahoo.com.br>
2026-02-21 13:37:43 -0300

    implements project nav component

---
+++ app/components/layouts/ProjectNav.vue
@@ -0,0 +1,37 @@
+<template>
+  <Navbar expand="lg" background-color="body-tertiary">
+    <Container type="fluid">
+      <NavbarBrand to="/">{{ useRuntimeConfig().public.appName }}</NavbarBrand>
+      <NavbarToggler />
+      <NavbarCollapse>
+        <NavbarNavList margin="e-auto b-2 b-lg-0">
+          <NavItem>
+            <NavLink to="/projetos">
+              Projetos
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/" @click.prevent="signout">
+              Sair
+            </NavLink>
+          </NavItem>
+        </NavbarNavList>
+      </NavbarCollapse>
+    </Container>
+  </Navbar>
+</template>
+
+<script lang="ts" setup>
+  const {clear} = useUserSession();
+  const toast = useMyToastStore();
+
+
+  async function signout(){
+    clear()
+    await toast.load('Sessão encerrada com sucesso', 'alerta')
+  }
+</script>
+
+<style>
+
+</style>
